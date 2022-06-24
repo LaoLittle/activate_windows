92b2ecadfcf2de73005a1c6648cac1e6f6d1368e
LaoLittle <y1941803875@outlook.com>
2022-06-25 03:01:00 +0800

    use system font

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
-pub fn get_display_size() -> (u64, u64) {
+pub fn get_display_size() -> (u32, u32) {
     #[cfg(target_os = "macos")]
     {
         use core_graphics::display::CGDisplay;
         let cg = CGDisplay::main();
 
-        return (cg.pixels_wide(), cg.pixels_high());
+        return (cg.pixels_wide() as u32, cg.pixels_high() as u32);
     }
 
     #[cfg(not(target_os = "macos"))]
@@ -13,5 +13,22 @@ pub fn get_display_size() -> (u64, u64) {
         let size = lp.primary_monitor().unwrap().size();
 
         return (size.width, size.height);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use font_kit::source::SystemSource;
+
+    #[test]
+    fn font() {
+        let mut s = SystemSource::new()
+            .select_family_by_name("Microsoft Yahei").unwrap()
+            .fonts()
+            .first()
+            .unwrap()
+            .load().unwrap();
+
+
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use std::env;
 use std::error::Error;
+use font_kit::source::SystemSource;
 
-use iced::{Application, Color, Column, Command, Element, Settings, Text, window};
+use iced::{Application, Color, Column, Command, Element, Font, Settings, Text, window};
 use iced::window::Position;
 
 use activate_windows::get_display_size;
-
-static FONT: &[u8] = include_bytes!("../MiSans-Regular.ttf");
 
 fn main() -> Result<(), Box<dyn Error>> {
     {
@@ -21,25 +20,49 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     }
 
-    let out = std::process::Command::new(env::current_exe()?)
-        .args(&["get_size"]).output()?;
+    let width: u32;
+    let height;
 
-    let string = String::from_utf8(out.stdout)?;
+    let font_size;
+    {
+        let out = std::process::Command::new(env::current_exe()?)
+            .args(&["get_size"]).output()?;
 
-    let sli: Vec<&str> = string.split('x').collect();
+        let string = String::from_utf8(out.stdout)?;
 
-    let width: u32 = sli[0].parse()?;
-    let height: u32 = sli[1].parse()?;
+        let sli: Vec<&str> = string.split('x').collect();
 
-    let font_size = width.min(height) >> 4;
+        width = sli[0].parse()?;
+        height = sli[1].parse()?;
 
-    println!("{:?}", (width, height));
+        font_size = width.min(height) >> 4;
+    }
+
+    //println!("{:?}", (width, height));
+
+    let font;
+    {
+        let f = SystemSource::new();
+
+        #[cfg(target_os = "windows")]
+            let f = f.select_family_by_name("Microsoft Yahei")?;
+
+        #[cfg(target_os = "macos")]
+            let f = f.select_family_by_name("PingFang SC")?;
+
+        let f = f.fonts().first().unwrap()
+        .load()?.copy_font_data().unwrap();
+
+        font = f;
+    }
+
+    let font = Box::leak(Box::new(font));
 
     ActivateWindows::run(
         Settings {
             id: None,
             window: window::Settings {
-                size: (width >> 2, font_size),
+                size: ((font_size << 4) / 3, font_size),
                 position: Position::Specific(((width * 3) >> 2) as _, ((height * 3) >> 2) as _),
                 min_size: None,
                 max_size: None,
@@ -50,7 +73,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                 icon: None,
             },
             flags: (),
-            default_font: Some(FONT),
+            default_font: Some(font),
             default_text_size: (font_size >> 1) as u16,
             text_multithreading: false,
             antialiasing: true,
